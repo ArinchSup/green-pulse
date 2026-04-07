c8068cf385971817ad4eb4b80116206ab52c0120
Peach <peach@Peachs-MacBook-Air.local>
2026-04-07 15:48:15 +0700

    structuring simple website green theme

---
+++ src/webInterface/src/variable.ts
@@ -0,0 +1,7 @@
+export const NavItems = [
+    { id: "overview", label: "Overview"},
+    { id: "portfolio", label: "Portfolio"},
+    { id: "watchlist", label: "Watchlist"},
+    { id: "news", label: "News Feed"},
+    { id: "settings", label: "Settings"},
+]
