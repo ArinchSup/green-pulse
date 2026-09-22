996806e89931973d5073ae3b2963100830beaf69
Arinchai <arinchwork@gmail.com>
2026-09-22 21:36:37 +0700

    Delete terraform directory
    Migrate infra

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "Public_IP" {
-  value = aws_instance.app.public_ip
-}
-
-output "Instance_ID" {
-  value = aws_instance.app.id
-}
--- terraform/vpc.tf
+++
@@ -1,7 +0,0 @@
-data "aws_vpc" "main" {
-    id = var.vpc_id
-}
-
-data "aws_subnet" "main" {
-    id = var.subnet_id
-}
